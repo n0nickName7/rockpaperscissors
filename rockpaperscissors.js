7121af0c286f55ca5c9d491760cc8e449af5ec75
n0nickName7 <118496059+n0nickName7@users.noreply.github.com>
2023-05-13 18:07:57 -0500

    I did not do good at commiting sorry

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,4 @@
+      
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
@@ -11,32 +12,153 @@ function getComputerChoice() {
  
  let playerScore = 0
  let comptScore = 0
+
+
  
+
+const buttons = document.querySelectorAll("button");
+
+const r = document.getElementById("rock")
+const p = document.getElementById("paper")
+const s = document.getElementById("scissors")
+
+function getButton() {
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      let playerSelection;
+      if(button.id == "rock") playerSelection = "rock";
+      if(button.id == "paper") playerSelection = "paper";
+      if(button.id == "scissors") playerSelection = "scissors";
+
+      const computerSelection = getComputerChoice();
+    
+    
+      playRound(playerSelection, computerSelection);
+      
+
+      document.getElementById('yourScore').innerHTML = "Your score: " + playerScore;
+      document.getElementById('comptScore').innerHTML = "Computers score: " + comptScore;
+      document.getElementById('comptChoice').innerHTML = "Computer Chose: " + computerSelection;
+      document.getElementById('yourChoice').innerHTML = "You Chose: " + playerSelection;
+
+      document.getElementById('roundWinner').innerHTML = declareWinner() + " this round";
+      
+      
+      isGameOver();
  
- function playRound(playerSelection, computerSelection) {
-   if (playerSelection === computerSelection) {
+     }) 
+  })
+}
+
+getButton()
+
+function displayGameover() {
+  const gameover = document.createElement('div');
+  gameover.classList.add('gameover');
+  gameover.innerHTML = 'Game Over';
+  document.body.appendChild(gameover);
+}
+
+
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    roundWinner = "tie"
+  }
+  if( (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'rock')) {
+       playerScore ++
+       roundWinner = "player"
+      
+    }
+  if ((computerSelection === 'rock' && playerSelection === 'scissors') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper') ||
+    (computerSelection === 'paper' && playerSelection === 'rock') ){
+       comptScore ++
+       roundWinner = "computer"
+      
+    } else {
+      return 'error'
+    }
+}
+
+
+function declareWinner() {
+  if (roundWinner === 'tie') {
      return "it's a tie"
-   }
-   if( (playerSelection === 'rock' && computerSelection === 'scissors') ||
-     (playerSelection === 'scissors' && computerSelection === 'paper') ||
-     (playerSelection === 'paper' && computerSelection === 'rock')) {
-        playerScore ++
-        return "you won"
-       
-     }
-   if ((computerSelection === 'rock' && playerSelection === 'scissors') ||
-     (computerSelection === 'scissors' && playerSelection === 'paper') ||
-     (computerSelection === 'paper' && playerSelection === 'rock') ){
-        comptScore ++
-        return "computer won"
-       
-     } else {
-       return 'error'
-     }
- }
+  } else if (roundWinner === "player") {
+    return "you won"
+  } else if (roundWinner === "computer") {
+    return "you lost"
+  } else {
+    return "error"
+  }
+}
+
+
+
+function isGameOver() {
+  const gameWinner = document.createElement('div')
+  gameWinner.classList.add('gameWinner')
+
+  if (playerScore === 5) {
+    gameWinner.innerHTML = "You Won the Game";
+    displayGameover();
+  } else if (comptScore === 5) {
+    gameWinner.innerHTML = "You Lost the Game";
+    displayGameover();
+  } else {
+    return "error";
+  }
+  document.body.appendChild(gameWinner);
+  reset()
+
+}
+
+
+function reset() {
+  const replayButton = document.createElement('button');
+  replayButton.classList.add('replay');
+  replayButton.innerHTML = "Replay?"
+  document.body.appendChild(replayButton)
   
+}
+
+
+
+
+
+
+/*function play(playerSelection) {
+
+r.addEventListener("click", () => {
+  playerSelection = "rock"
+});
+
+p.addEventListener("click", () => {
+  playerSelection="paper"
+});
+
+s.addEventListener("click", () => {
+  playerSelection="scissors"
+});
+
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
+
+
+playRound(playerSelection, computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+}
+
+play()*/
+
+
  
- for (let i = 0; i < 5; i++) {
+ /* for (let i = 0; i < 5; i++) {
     const playerSelection = prompt('Pick a move');
     console.log(playerSelection)
     const computerSelection = getComputerChoice();
@@ -44,17 +166,6 @@ function getComputerChoice() {
     console.log(playRound(playerSelection, computerSelection));
     console.log(playerScore);
     console.log(comptScore);
-    }
-    
-    function declareWinner() {
-        if (playerScore === comptScore) {
-            return "no one won"
-        } 
-        if (playerScore > comptScore) {
-            return "you won the game"
-        } else {
-            return "you lost the game"
-        }
-    }
+    } */
 
-    console.log(declareWinner());
+
